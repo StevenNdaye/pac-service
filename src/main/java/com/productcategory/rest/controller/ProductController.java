a72ce5aa1f5a90cb0978d3c57986cbf22b627b7e
StevenNdaye <stevenndaye@gmail.com>
2014-11-03 18:30:01 +0200

    Refactored the controller

--- src/main/java/com/productcategory/rest/controller/ProductController.java
+++ src/main/java/com/productcategory/rest/controller/ProductController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
  * Created by steven on 2014/11/02.
  */
 @RestController
+@RequestMapping(value = "/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -21,27 +22,27 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public Iterable<Product> getProducts() {
         return productService.getProducts();
     }
 
-    @RequestMapping(value = "/products/{productId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
     public Product getProduct(@PathVariable int productId) throws ProductNotFoundException {
         return this.productService.getProduct(productId);
     }
 
-    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public Product saveProduct(@RequestBody @Valid Product product) {
         return productService.saveProduct(product);
     }
 
-    @RequestMapping(value = "/products/{productId}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{productId}", method = RequestMethod.DELETE)
     public String deleteProduct(@PathVariable int productId) {
         return productService.deleteProduct(productId);
     }
 
-    @RequestMapping(value = "/products/{productId}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{productId}", method = RequestMethod.PUT)
     public Product updateProduct(@PathVariable int productId, @RequestBody @Valid Product product) {
         return productService.updateProduct(productId, product);
     }
